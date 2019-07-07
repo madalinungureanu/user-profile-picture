d0533fe8a384a44bb3e3e895c03de18e9fc8a883
Ronald Huereca <ronald@mediaron.com>
2019-07-06 23:49:58 -0500

    Code cleanup.

--- metronet-profile-picture.php
+++ metronet-profile-picture.php
@@ -1,4 +1,4 @@
-<?php
+<?php // phpcs:ignore
 /*
 Plugin Name: User Profile Picture
 Plugin URI: http://wordpress.org/extend/plugins/metronet-profile-picture/
@@ -77,7 +77,6 @@ class Metronet_Profile_Picture {
 		add_action( 'edit_user_profile_update', array( $this, 'save_user_profile' ) );
 		add_action( 'personal_options_update', array( $this, 'save_user_profile' ) );
 
-
 		//User Avatar override
 		add_filter( 'get_avatar', array( $this, 'avatar_override' ), 10, 6 );
 		add_filter( 'pre_get_avatar_data', array( $this, 'pre_avatar_override' ), 10, 2 );
@@ -92,7 +91,7 @@ class Metronet_Profile_Picture {
 		$options = $this->get_options();
 		if ( 'on' === $options['load_gutenberg'] ) {
 			// Include Gutenberg
-			add_filter( 'block_categories', array( $this, 'add_block_category' ), 10, 2);
+			add_filter( 'block_categories', array( $this, 'add_block_category' ), 10, 2 );
 			include_once self::get_plugin_dir( '/gutenberg/class-gutenberg.php' );
 			new Metronet_Profile_Picture_Gutenberg();
 		}
@@ -159,7 +158,13 @@ class Metronet_Profile_Picture {
 				<?php wp_nonce_field( 'save_mpp_options' ); ?>
 				<h1><svg id="Layer_1" width="30" height="30" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 753.53 979.74"><title>upp</title><path d="M806.37,185.9c0,40.27-30.49,72.9-68.11,72.9s-68.17-32.63-68.17-72.9S700.62,113,738.26,113,806.37,145.64,806.37,185.9Z" transform="translate(-123.47 -11)" fill="#4063ad"/><path d="M330.36,183.8c0,40.27-30.49,72.9-68.12,72.9s-68.17-32.63-68.17-72.9,30.52-72.87,68.17-72.87S330.36,143.56,330.36,183.8Z" transform="translate(-123.47 -11)" fill="#a34d9c"/><path d="M331.3,888.13V698.21H329c-31.64,0-57.28-27.45-57.28-61.29V336.5a118.37,118.37,0,0,1,5.43-34.79H179.84c-31.94,0-56.37,31.57-56.37,56.34V601.46h48.32V888.13Z" transform="translate(-123.47 -11)" fill="#a34d9c"/><path d="M388.59,636.92V990.74H611.88V636.92H671.5V336.5c0-30.63-27.64-69.57-69.6-69.57H398.56c-39.44,0-69.61,38.94-69.61,69.57V636.92Z" transform="translate(-123.47 -11)" fill="#f4831f"/><path d="M584.3,101c0,49.69-37.63,90-84,90S416.12,150.67,416.12,101s37.66-90,84.14-90S584.3,51.27,584.3,101Z" transform="translate(-123.47 -11)" fill="#f4831f"/><path d="M820.61,303.79H724.08a121.69,121.69,0,0,1,4.7,32.71V636.92c0,33.84-25.64,61.29-57.28,61.29h-2.33v192H828.7V603.54H877V360.16C877,335.36,854.62,303.79,820.61,303.79Z" transform="translate(-123.47 -11)" fill="#4063ad"/></svg> <?php esc_html_e( 'User Profile Picture', 'metronet-profile-picture' ); ?></h1>
 				<p><?php esc_html_e( 'Welcome to User Profile Picture!', 'metronet-profile-picture' ); ?></p>
-				<p><?php esc_html_e( 'Please try: ', 'metronet-profile-picture' ); ?><a target="_blank" href="https://mediaron.com/downloads/user-profile-picture-enhanced/"><?php esc_html_e( 'User Profile Picture Enhanced', 'metronet-profile-picture' ); ?></a></p>
+				<?php
+				if ( ! defined( 'USER_PROFILE_PICTURE_ENHANCED' ) ) :
+					?>
+					<p><?php esc_html_e( 'Please try: ', 'metronet-profile-picture' ); ?><a target="_blank" href="https://mediaron.com/downloads/user-profile-picture-enhanced/"><?php esc_html_e( 'User Profile Picture Enhanced', 'metronet-profile-picture' ); ?></a></p>
+					<?php
+				endif;
+				?>
 				<table class="form-table">
 					<tbody>
 						<tr>
@@ -724,11 +729,13 @@ class Metronet_Profile_Picture {
 			esc_url( admin_url( 'options-general.php?page=mpp' ) ),
 			esc_html__( 'Settings', 'metronet-profile-picture' )
 		);
-		$admin_anchor .= sprintf(
-			' | <a href="%s">%s</a>',
-			esc_url( 'https://mediaron.com/downloads/user-profile-picture-enhanced/' ),
-			esc_html__( 'Get Enhanced!', 'metronet-profile-picture' )
-		);
+		if ( ! defined( 'USER_PROFILE_PICTURE_ENHANCED' ) ) {
+			$admin_anchor .= sprintf(
+				' | <a href="%s">%s</a>',
+				esc_url( 'https://mediaron.com/downloads/user-profile-picture-enhanced/' ),
+				esc_html__( 'Get Enhanced!', 'metronet-profile-picture' )
+			);
+		}
 		if ( ! is_array( $settings ) ) {
 			return array( $admin_anchor );
 		} else {
@@ -778,7 +785,7 @@ class Metronet_Profile_Picture {
 					$remove_classes[] = 'mpp-no-profile-image';
 				}
 				?>
-					<a id="metronet-remove" class="<?php echo implode( ' ', $remove_classes ); ?>" href="#" title="<?php esc_attr_e( 'Remove profile image', 'metronet-profile-picture' ); ?>"><?php esc_html_e( 'Remove profile image', 'metronet-profile-picture' );?></a>
+					<a id="metronet-remove" class="<?php echo implode( ' ', $remove_classes ); // phpcs:ignore ?>" href="#" title="<?php esc_attr_e( 'Remove profile image', 'metronet-profile-picture' ); ?>"><?php esc_html_e( 'Remove profile image', 'metronet-profile-picture' ); ?></a>
 					<div style="display: none">
 						<?php printf( '<img class="mpp-loading" width="150" height="150" alt="Loading" src="%s" />', esc_url( self::get_plugin_url( '/img/loading.gif' ) ) ); ?>
 					</div>
@@ -803,7 +810,7 @@ class Metronet_Profile_Picture {
 						<?php
 						else :
 							?>
-							<br /><input type="checkbox" name="metronet-user-avatar" id="metronet-user-avatar" value="on" <?php echo $checked; ?> /><label for="metronet-user-avatar"> <?php esc_html_e( 'Override Avatar?', 'metronet-profile-picture' ); ?></label>
+							<br /><input type="checkbox" name="metronet-user-avatar" id="metronet-user-avatar" value="on" <?php echo $checked; // phpcs:ignore ?> /> <label for="metronet-user-avatar"><?php esc_html_e( 'Override Avatar?', 'metronet-profile-picture' ); ?></label>
 						<?php endif; ?>
 				</div><!-- #metronet-override-avatar -->
 			</td>
