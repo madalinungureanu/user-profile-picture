5d341ca9a27cf7f8f65ab5d7bdffd9b13da1ea19
Ronald Huereca <rhuereca@updraftplus.com>
2020-11-27 04:53:16 -0600

    Base foundation for tabs in the admin.

--- includes/admin/class-setup.php
+++ includes/admin/class-setup.php
@@ -177,6 +177,216 @@ class Setup {
 	}
 
 	/**
+	 * Registers and outputs placeholder for settings.
+	 *
+	 * @since 1.0.0
+	 */
+	public static function settings_page() {
+		?>
+		<div class="wrap upp-admin-wrap">
+			<?php
+			self::get_settings_header();
+			self::get_settings_tabs();
+			self::get_settings_footer();
+			?>
+		</div>
+		<?php
+	}
+
+	/**
+	 * Output the top-level admin tabs.
+	 */
+	public static function get_settings_tabs() {
+		$settings_url_base = Functions::get_settings_url( 'setup' )
+		?>
+			<?php
+			$tabs = array();
+			/**
+			 * Filer the output of the tab output.
+			 *
+			 * Potentially modify or add your own tabs.
+			 *
+			 * @since 1.0.0
+			 *
+			 * @param array $tabs Associative array of tabs.
+			 */
+			$tabs       = apply_filters( 'mpp_admin_tabs', $tabs );
+			$tab_html   = '<nav class="nav-tab-wrapper">';
+			$tabs_count = count( $tabs );
+			if ( $tabs && ! empty( $tabs ) && is_array( $tabs ) ) {
+				$active_tab = Functions::get_admin_tab();
+				if ( null === $active_tab ) {
+					$active_tab = 'setup';
+				}
+				$is_tab_match = false;
+				if ( 'setup' === $active_tab ) {
+					$active_tab = 'setup';
+				} else {
+					foreach ( $tabs as $tab ) {
+						$tab_get = isset( $tab['get'] ) ? $tab['get'] : '';
+						if ( $active_tab === $tab_get ) {
+							$is_tab_match = true;
+						}
+					}
+					if ( ! $is_tab_match ) {
+						$active_tab = 'setup';
+					}
+				}
+				$do_action = false;
+				foreach ( $tabs as $tab ) {
+					$classes = array( 'nav-tab' );
+					$tab_get = isset( $tab['get'] ) ? $tab['get'] : '';
+					if ( $active_tab === $tab_get ) {
+						$classes[] = 'nav-tab-active';
+						$do_action = isset( $tab['action'] ) ? $tab['action'] : false;
+					} elseif ( ! $is_tab_match && 'setup' === $tab_get ) {
+						$classes[] = 'nav-tab-active';
+						$do_action = isset( $tab['action'] ) ? $tab['action'] : false;
+					}
+					$tab_url   = isset( $tab['url'] ) ? $tab['url'] : '';
+					$tab_label = isset( $tab['label'] ) ? $tab['label'] : '';
+					$tab_html .= sprintf(
+						'<a href="%s" class="%s" id="eff-%s"><svg class="mpp-icon mpp-icon-tab">%s</svg><span>%s</span></a>',
+						esc_url( $tab_url ),
+						esc_attr( implode( ' ', $classes ) ),
+						esc_attr( $tab_get ),
+						sprintf( '<use xlink:href="#%s"></use>', esc_attr( $tab['icon'] ) ),
+						esc_html( $tab['label'] )
+					);
+				}
+				$tab_html .= '</nav>';
+				if ( $tabs_count > 0 ) {
+					echo wp_kses( $tab_html, Functions::get_kses_allowed_html() );
+				}
+
+				$current_tab     = Functions::get_admin_tab();
+				$current_sub_tab = Functions::get_admin_sub_tab();
+
+				/**
+				 * Filer the output of the sub-tab output.
+				 *
+				 * Potentially modify or add your own sub-tabs.
+				 *
+				 * @since 3.0.0
+				 *
+				 * @param array Associative array of tabs.
+				 * @param string Tab
+				 * @param string Sub Tab
+				 */
+				$sub_tabs = apply_filters( 'mpp_admin_sub_tabs', array(), $current_tab, $current_sub_tab );
+
+				// Check to see if no tabs are available for this view.
+				if ( null === $current_tab && null === $current_sub_tab ) {
+					$current_tab = 'setup';
+				}
+				if ( $sub_tabs && ! empty( $sub_tabs ) && is_array( $sub_tabs ) ) {
+					if ( null === $current_sub_tab ) {
+						$current_sub_tab = '';
+					}
+					$is_tab_match      = false;
+					$first_sub_tab     = current( $sub_tabs );
+					$first_sub_tab_get = $first_sub_tab['get'];
+					if ( $first_sub_tab_get === $current_sub_tab ) {
+						$active_tab = $current_sub_tab;
+					} else {
+						$active_tab = $current_sub_tab;
+						foreach ( $sub_tabs as $tab ) {
+							$tab_get = isset( $tab['get'] ) ? $tab['get'] : '';
+							if ( $active_tab === $tab_get ) {
+								$is_tab_match = true;
+							}
+						}
+						if ( ! $is_tab_match ) {
+							$active_tab = $first_sub_tab_get;
+						}
+					}
+					$sub_tab_html_array = array();
+					$do_subtab_action   = false;
+					$maybe_sub_tab      = '';
+					foreach ( $sub_tabs as $sub_tab ) {
+						$classes = array( 'mpp-sub-tab' );
+						$tab_get = isset( $sub_tab['get'] ) ? $sub_tab['get'] : '';
+						if ( $active_tab === $tab_get ) {
+							$classes[]        = 'mpp-sub-tab-active';
+							$do_subtab_action = true;
+							$current_sub_tab  = $tab_get;
+						} elseif ( ! $is_tab_match && $first_sub_tab_get === $tab_get ) {
+							$classes[]        = 'mpp-sub-tab-active';
+							$do_subtab_action = true;
+							$current_sub_tab  = $first_sub_tab_get;
+						}
+						$tab_url   = isset( $sub_tab['url'] ) ? $sub_tab['url'] : '';
+						$tab_label = isset( $sub_tab['label'] ) ? $sub_tab['label'] : '';
+						if ( $current_sub_tab === $tab_get ) {
+							$sub_tab_html_array[] = sprintf( '<span class="%s" id="mpp-tab-%s">%s</span>', esc_attr( implode( ' ', $classes ) ), esc_attr( $tab_get ), esc_html( $sub_tab['label'] ) );
+						} else {
+							$sub_tab_html_array[] = sprintf( '<a href="%s" class="%s" id="mpp-tab-%s">%s</a>', esc_url( $tab_url ), esc_attr( implode( ' ', $classes ) ), esc_attr( $tab_get ), esc_html( $sub_tab['label'] ) );
+						}
+					}
+					if ( ! empty( $sub_tab_html_array ) ) {
+						echo '<nav class="mpp-sub-links">' . wp_kses_post( rtrim( implode( ' | ', $sub_tab_html_array ), ' | ' ) ) . '</nav>';
+					}
+					if ( $do_subtab_action ) {
+						/**
+						 * Perform a sub tab action.
+						 *
+						 * Perform a sub tab action. Useful for loading scripts or inline styles as necessary.
+						 *
+						 * @since 3.0.0
+						 *
+						 * mpp_admin_sub_tab_{current_tab}_{current_sub_tab}
+						 * @param string Sub Tab
+						 */
+						do_action(
+							sprintf( // phpcs:ignore
+								'mpp_admin_sub_tab_%s_%s',
+								sanitize_title( $current_tab ),
+								sanitize_title( $current_sub_tab )
+							)
+						);
+					}
+				}
+				if ( $do_action ) {
+
+					/**
+					 * Perform a tab action.
+					 *
+					 * Perform a tab action.
+					 *
+					 * @since 3.0.0
+					 *
+					 * @param string $action Can be any action.
+					 * @param string Tab
+					 * @param string Sub Tab
+					 */
+					do_action( $do_action, $current_tab, $current_sub_tab );
+				}
+			}
+			?>
+		<?php
+	}
+
+	/**
+	 * Output Admin Page Header.
+	 */
+	public static function get_settings_header() {
+		?>
+		<div class="wrap upp-admin-wrap">
+			<h1><strong>User Profile</strong> Picture</h1>
+			<p class="upp-info-text"><?php esc_html_e( 'The easiest way to add a profile picture for your users.', 'metronet-profile-picture' ); ?></p>
+		<?php
+	}
+
+	/**
+	 * Run script and enqueue stylesheets and stuff like that.
+	 */
+	public static function get_settings_footer() {
+		?>
+		</div><!-- .wrap.upp-admin-wrap -->
+		<?php
+	}
+
+	/**
 	 * Register the settings menu for User Profile Picture
 	 *
 	 * @since 2.3.0
@@ -188,7 +398,7 @@ class Setup {
 				__( 'User Profile Picture', 'metronet-profile-picture' ),
 				'manage_options',
 				'mpp',
-				array( $this, 'admin_page' ),
+				array( '\MPP\Includes\Admin\Setup', 'settings_page' ),
 				'dashicons-groups',
 				100
 			);
@@ -198,7 +408,7 @@ class Setup {
 				__( 'Profile Picture', 'metronet-profile-picture' ),
 				'manage_options',
 				'mpp',
-				array( $this, 'admin_page' )
+				array( '\MPP\Includes\Admin\Setup', 'settings_page' )
 			);
 		}
 	}
